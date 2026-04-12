d9735cfc7b21ccd9981d14947e508395c8dbc39e
Nauman Nasir <nauman.nasir@iskaan.com>
2026-04-12 14:40:33 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -209,7 +209,11 @@ export default function Home() {
             </svg>
           ), color: 'text-white', bg: 'bg-white/10' },
           { name: 'Facebook', icon: Facebook, color: 'text-blue-600', bg: 'bg-blue-600/10' },
-          { name: 'Twitter', icon: Twitter, color: 'text-sky-400', bg: 'bg-sky-400/10' }
+          { name: 'X (Twitter)', icon: ({ className }: any) => (
+            <svg className={className} viewBox="0 0 1200 1227" fill="currentColor">
+              <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"/>
+            </svg>
+          ), color: 'text-white', bg: 'bg-white/10' }
         ].map((platform) => (
             <motion.div 
               key={platform.name}
